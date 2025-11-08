3291b399e0d12d4e42a36f294624ad9e2c5a9554
Jurica Čavlek <jurica.cavlek@gmail.com>
2025-11-08 17:48:30 -0600

    Added check for payment

--- java/src/main/java/io/fliqa/hackaton/infrastructure/web/WalletResource.java
+++ java/src/main/java/io/fliqa/hackaton/infrastructure/web/WalletResource.java
@@ -5,13 +5,14 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.QueryParam;
 
 @ApplicationScoped
 @Path("/wallet")
 public class WalletResource {
 
     @GET
-    public String getWallet(@NotNull @NotEmpty String walletId) {
+    public String getWallet(@QueryParam("address") @NotNull @NotEmpty String address) {
         return "Wallet";
     }
 }
