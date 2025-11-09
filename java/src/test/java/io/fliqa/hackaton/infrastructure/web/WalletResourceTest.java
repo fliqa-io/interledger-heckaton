dd1a5a188aadab0e243c3068ace438d599cfbea4
Jurica Čavlek <jurica.cavlek@gmail.com>
2025-11-08 21:10:53 -0600

    Fixed tests

--- java/src/test/java/io/fliqa/hackaton/infrastructure/web/WalletResourceTest.java
+++ java/src/test/java/io/fliqa/hackaton/infrastructure/web/WalletResourceTest.java
@@ -1,12 +1,14 @@
 package io.fliqa.hackaton.infrastructure.web;
 
 import io.fliqa.hackaton.infrastructure.web.dto.WalletData;
+import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+@QuarkusTest
 class WalletResourceTest {
 
     @Test
